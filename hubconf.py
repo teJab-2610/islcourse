a6e2990447bf319760e70328949d6b710087d644
Kalidas Yeturu <31208384+ykalidasiittp@users.noreply.github.com>
2022-10-19 05:09:10 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -15,4 +15,18 @@ def get_model(train_data=None, n_epochs=10):
   
   return model
   
+# sample invocation torch.hub.load(myrepo,'test_model',model=model1,test_data=test_data,force_reload=True)
+def test_model(model=model, test_data=test_data):
+
+  accuracy_val, precision_val, recall_val, f1score_val = 0, 0, 0, 0
+  # write your code here as per instructions
+  # ... your code ...
+  # ... your code ...
+  # ... and so on ...
+  # calculate accuracy, precision, recall and f1score
+  
+  print ('Returning metrics... (rollnumber: xx)')
+  
+  return accuracy_val, precision_val, recall_val, f1score_val
+
 
