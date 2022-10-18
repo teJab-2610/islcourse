81a8b9a506b5ed1ee550ca93a9a5fecd4988a355
Kalidas Yeturu <31208384+ykalidasiittp@users.noreply.github.com>
2022-10-19 05:04:52 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,2 +1,18 @@
 def kali():
   print ('kali')
+  
+# sample invocation torch.hub.load(myrepo,'get_model',train_data=train_data,n_epochs=5, force_reload=True)
+def get_model(train_data=None, n_epochs=10):
+  model = None
+
+  # write your code here as per instructions
+  # ... your code ...
+  # ... your code ...
+  # ... and so on ...
+  # set model variable to proper object, make use of train_data
+  
+  print ('Returning model... (rollnumber: xx)')
+  
+  return model
+  
+
