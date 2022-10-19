f245d020e22f850ddcf602ce125431dd396b222b
Kalidas Yeturu <31208384+ykalidasiittp@users.noreply.github.com>
2022-10-19 06:01:07 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -18,7 +18,32 @@ def get_model(train_data=None, n_epochs=10):
   # ... your code ...
   # ... your code ...
   # ... and so on ...
+  # Use softmax and cross entropy loss functions
   # set model variable to proper object, make use of train_data
+  
+  print ('Returning model... (rollnumber: xx)')
+  
+  return model
+
+# sample invocation torch.hub.load(myrepo,'get_model_advanced',train_data=train_data,n_epochs=5, force_reload=True)
+def get_model_advanced(train_data=None, n_epochs=10,lr=1e-4,config=None):
+  model = None
+
+  # write your code here as per instructions
+  # ... your code ...
+  # ... your code ...
+  # ... and so on ...
+  # Use softmax and cross entropy loss functions
+  # set model variable to proper object, make use of train_data
+  
+  # In addition,
+  # Refer to config dict, where learning rate is given, 
+  # List of (in_channels, out_channels, kernel_size, stride=1, padding='same')  are specified
+  # Example, config = [(1,10,(3,3),1,'same'), (10,3,(5,5),1,'same'), (3,1,(7,7),1,'same')], it can have any number of elements
+  # You need to create 2d convoution layers as per specification above in each element
+  # You need to add a proper fully connected layer as the last layer
+  return model
+  
   
   print ('Returning model... (rollnumber: xx)')
   
