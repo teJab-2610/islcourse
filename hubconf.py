820402d2f434635734e79862ef924d52f8e078c3
Kalidas Yeturu <31208384+ykalidasiittp@users.noreply.github.com>
2022-11-20 23:05:47 +0530

    Added Part 3 - pytorch autoencoder and predictor
    The neural network has both autoencoder and predictor as part of two components of a loss function.
    
    First component is encoded vector to predicted class category error and,
    Second component is X reconstruction error.
    
    Both the components of the loss have to be minimized.

--- hubconf.py
+++ hubconf.py
@@ -1,4 +1,7 @@
 # kali
+import torch
+from torch import nn
+import torch.optim as optim
 
 # You can import whatever standard packages are required
 
@@ -118,6 +121,78 @@ def perform_gridsearch_cv_multimetric(model=None, param_grid=None, cv=5, X=None,
   return top1_scores
 
 ###### PART 3 ######
+
+class MyNN(nn.Module):
+  def __init__(self,inp_dim=64,hid_dim=13,num_classes=10):
+    super(MyNN,self)
+    
+    self.fc_encoder = None # write your code inp_dim to hid_dim mapper
+    self.fc_decoder = None # write your code hid_dim to inp_dim mapper
+    self.fc_classifier = None # write your code to map hid_dim to num_classes
+    
+    self.relu = None #write your code - relu object
+    self.softmax = None #write your code - softmax object
+    
+  def forward(self,x):
+    x = None # write your code - flatten x
+    x_enc = self.fc_encoder(x)
+    x_enc = self.relu(x_enc)
+    
+    y_pred = self.fc_classifier(x_enc)
+    y_pred = self.softmax(y_pred)
+    
+    x_dec = self.fc_decoder(x_enc)
+    
+    return y_pred, x_dec
+  
+  # This a multi component loss function - lc1 for class prediction loss and lc2 for auto-encoding loss
+  def loss_fn(self,x,yground,y_pred,xencdec):
+    
+    # class prediction loss
+    # yground needs to be one hot encoded - write your code
+    lc1 = None # write your code for cross entropy between yground and y_pred, advised to use torch.mean()
+    
+    # auto encoding loss
+    lc2 = torch.mean((x - xencdec)**2)
+    
+    lval = lc1 + lc2
+    
+    return lval
+    
+def get_mynn(inp_dim=64,hid_dim=13,num_classes=10):
+  mynn = MyNN(inp_dim,hid_dim,num_classes)
+  mynn.double()
+  return mynn
+
+def get_mnist_tensor():
+  # download sklearn mnist
+  # convert to tensor
+  X, y = None, None
+  # write your code
+  return X,y
+
+def get_loss_on_single_point(mynn=None,x0,y0):
+  y_pred, xencdec = mynn(x0)
+  lossval = mynn.loss_fn(x0,y0,y_pred,xencdec)
+  # the lossval should have grad_fn attribute set
+  return lossval
+
+def train_combined_encdec_predictor(mynn=None,X,y, epochs=11):
+  # X, y are provided as tensor
+  # perform training on the entire data set (no batches etc.)
+  # for each epoch, update weights
+  
+  optimizer = optim.SGD(mynn.parameters(), lr=0.01)
+  
+  for i in range(epochs):
+    optimizer.zero_grad()
+    ypred, Xencdec = mynn(X)
+    lval = mynn.loss_fn(X,y,ypred,Xencdec)
+    lval.backward()
+    optimzer.step()
+    
+  return mynn
+    
 
 
 
